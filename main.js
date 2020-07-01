0adfc2b2eaad00e763b4eb0cb397350b33ed9b8d
Sarah Elhwarii <56038677+sarahelhwarii@users.noreply.github.com>
2020-07-01 15:02:20 +0300

    add random colors to squares

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const container = document.getElementById("container");
 const squares_no = 500;
-
+const colors = ["#e74c3c", "#8e44ad", "#e67e22", "#2ecc71", "#3498db"];
 for (let i = 0; i < squares_no; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
-
+  square.addEventListener("mouseover", () => {
+    const color = getRandomColor();
+    square.style.backgroundColor = color;
+  });
   container.appendChild(square);
+}
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
